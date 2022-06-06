a4bf646e59ffd1a1848d5ffdbfe3a06b86097558
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2022-06-06 20:50:30 +0200

    Redid everything

---
+++ SeleniumWebDriverTest/ColoredLine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Colored
+{
+    /// <summary>
+    /// Class for console input/output with coloring support
+    /// </summary>
+    static class ColoredLine
+    {
+        /// <summary>
+        /// Asks the user if he wants to execute some part of the code
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static bool YesOrNo(string message = "", ConsoleColor color = ConsoleColor.Gray)
+        {
+            Write($"{message} (y or n)", color);
+
+            string[] _tempYes = new string[] { "y", "Y", "yes", "Yes" };
+            string _temp = Console.ReadLine();
+            if (_tempYes.Contains(_temp))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Custom Console.WriteLine() function with line coloring support
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        public static void Write(string message = "", ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Custom function that writes a string and returns converted value entered by the user
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static T WriteAndRead<T>(T defaultValue = default, string message = "", ConsoleColor color = ConsoleColor.Gray)
+        {
+            Write(message, color);
+            // If the value of the variable is null, then the function returns an converted string. 
+
+            T _tempValue = defaultValue;
+            string _tempRL = Console.ReadLine();
+            if (_tempRL != string.Empty)
+            {
+                try
+                {
+                    _tempValue = (T)Convert.ChangeType(_tempRL, typeof(T));
+                }
+                catch (Exception ex)
+                {
+                    Write($"{ex}", ConsoleColor.Red);
+                }
+            }
+
+            return _tempValue;
+        }
+    }
+}
--- SeleniumWebDriverTest/Program.cs
+++ SeleniumWebDriverTest/Program.cs
@@ -1,27 +1,108 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.ObjectModel;
+using Colored;
 
 namespace SeleniumWebDriverTest
 {
+    /// <summary>
+    /// SeleniumWebDriverTest main class
+    /// </summary>
     static class Program
     {
+        /// <summary>
+        /// Chrome driver
+        /// </summary>
         static IWebDriver driver;
 
+        /// <summary>
+        /// Dropdown department
+        /// </summary>
         static string department = "Research & Development";
-        static int expectedResult = 15;
+        /// <summary>
+        /// Expected number of vacancies
+        /// </summary>
+        static uint expectedResult = 15;
+        /// <summary>
+        /// Timeout for the page to load
+        /// </summary>
         static uint timeout = 10;
 
-        static void Main(string[] args)
+        /// <summary>
+        /// Main function
+        /// </summary>
+        static void Main()
+        {
+            InitVariables();
+            InitDriver();
+
+            ChooseLanguage();
+
+            GoToJobs();
+
+            ChooseDepartment();
+            FindVacancies();
+        }
+
+        #region Init
+
+        /// <summary>
+        /// Initialization of all necessary variables entered by the user
+        /// </summary>
+        static void InitVariables()
+        {
+
+            if (!ColoredLine.YesOrNo("Change input parameters?", ConsoleColor.DarkMagenta))
+                return;
+
+            ColoredLine.Write("\nIf the field is left blank, the values will not change.\n", ConsoleColor.DarkMagenta);
+
+            department = ColoredLine.WriteAndRead(department, $"Enter {nameof(department)} in {department.GetType()} type");
+            expectedResult = ColoredLine.WriteAndRead(expectedResult, $"Enter {nameof(expectedResult)} in {expectedResult.GetType()} type");
+            timeout = ColoredLine.WriteAndRead(timeout, $"Enter {nameof(timeout)} in {timeout.GetType()} type");
+        }
+
+        /// <summary>
+        /// Driver initialization and configuration
+        /// </summary>
+        static void InitDriver()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://careers.veeam.com/vacancies");
+            driver.Navigate().GoToUrl("https://cz.careers.veeam.com/vacancies");
+        }
 
-            //timeout = Convert.ToUInt32(Console.ReadLine());
+        #endregion
 
+        /// <summary>
+        /// Page language selection
+        /// </summary>
+        static void ChooseLanguage()
+        {
+            IWebElement dropBox = driver.FindElement(By.XPath("//*[@id='root']/div/header/div[1]/div/div/div/nav/nav/div/div"), timeout);
+            dropBox.Click();
+
+            IWebElement dropItem = dropBox.FindChildElementByName(By.ClassName("dropdown-item"), "Global", timeout);
+            dropItem.Click();
+        }
+
+        /// <summary>
+        /// Transition to the department of vacancies
+        /// </summary>
+        static void GoToJobs()
+        {
+            // It's is used to set time to wait for loading of the page.
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(timeout);
+            driver.FindElement(By.CssSelector("button[class='btn btn-apply btn-success']"), timeout).Click();
+        }
+
+        /// <summary>
+        /// Selecting a job filter
+        /// </summary>
+        static void ChooseDepartment()
+        {
             IWebElement dropdownBox = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/header/div/div/div[2]/div"), timeout);
             dropdownBox.Click();
 
@@ -29,28 +110,50 @@ namespace SeleniumWebDriverTest
             dropdownItem.Click();
 
             driver.FindElement(By.CssSelector("button[class='btn btn-outline-success']"), timeout).Click();
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(timeout);
-
-            ReadOnlyCollection<IWebElement> vacancies = driver.FindElement(By.CssSelector("div[class='d-none d-lg-block']")).FindElements(By.XPath("*"));
-
-            driver.FindElement(By.CssSelector("input[class='dropdown-toggle form-control']")).SendKeys($"found {vacancies.Count} vacancies");
-
-            Console.WriteLine(vacancies.Count);
-
-            if (vacancies.Count > expectedResult)
-            {
-                Console.WriteLine("succes!!!");
-            }
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeout);
         }
 
-        static IWebElement FindElement(this IWebDriver driver, By by, uint timeoutInSeconds)
+        /// <summary>
+        /// Finds the number of vacancies and compares them with the expected result
+        /// </summary>
+        static void FindVacancies()
+        {
+            ReadOnlyCollection<IWebElement> vacancies = driver.FindElement(By.CssSelector("div[class='d-none d-lg-block']"), timeout).FindElements(By.XPath("*"), timeout);
+
+            driver.FindElement(By.CssSelector("input[class='dropdown-toggle form-control']"), timeout).SendKeys("Look in the log");
+            ColoredLine.Write($"\nFound {vacancies.Count} vacancies", ConsoleColor.DarkMagenta);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeout);
+
+            if (vacancies.Count > expectedResult)
+                ColoredLine.Write($"Succes!!! This is more than expected vacancies by {vacancies.Count - expectedResult}!", ConsoleColor.Green);
+            else
+                ColoredLine.Write($"Failure((( This is less than expected vacancies by {expectedResult - vacancies.Count}(", ConsoleColor.Red);
+        }
+
+        #region FindElementWithTimeout
+
+        static IWebElement FindElement(this IWebDriver dr, By by, uint timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                // Webdriver waits until it times out then returns a variable.
+                WebDriverWait wait = new WebDriverWait(dr, TimeSpan.FromSeconds(timeoutInSeconds));
+
+                // sometimes gives an error. The solution is to increase the waiting time.
+                return wait.Until(drv => drv.FindElement(by));
+            }
+            return dr.FindElement(by);
+        }
+
+        static ReadOnlyCollection<IWebElement> FindElements(this IWebElement parent, By by, uint timeoutInSeconds)
         {
             if (timeoutInSeconds > 0)
             {
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-                return wait.Until(drv => drv.FindElement(by));
+                return wait.Until(drv => parent.FindElements(by));
             }
-            return driver.FindElement(by);
+            return driver.FindElements(by);
         }
 
         static IWebElement FindChildElementByName(this IWebElement parent, By by, string name, uint timeoutInSeconds = 10)
@@ -60,6 +163,7 @@ namespace SeleniumWebDriverTest
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
                 return wait.Until(drv =>
                 {
+                    // Find "first-line" parent element with the given name.
                     ReadOnlyCollection<IWebElement> webElements = parent.FindElements(by);
                     foreach (IWebElement element in webElements)
                     {
@@ -68,13 +172,14 @@ namespace SeleniumWebDriverTest
                             return element;
                         }
                     }
-                    
+
                     return null;
                 }
                 );
             }
-
             return parent.FindElement(by);
         }
+
+        #endregion
     }
 }
